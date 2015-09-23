e51b914fb0baf164b65dcf1828d58b42fc555037
kasunamarasinghe <kasun@davton.com>
2015-09-23 17:42:20 +0530

    completed

--- EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -14,8 +14,10 @@ public NinjectDependencyResolver() {
 kernel = new StandardKernel();
 AddBindings();
 }
-public object GetService(Type serviceType) {
-return kernel.TryGet(serviceType);
+public object GetService(Type serviceType)
+{
+    return kernel.TryGet(serviceType);
+}
     public IEnumerable<object> GetServices(Type serviceType) {
 return kernel.GetAll(serviceType);
 }
